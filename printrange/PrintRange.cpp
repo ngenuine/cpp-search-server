5de27e560c0458c686ed6d0d0ad103d1ff726b37
gr_nikita <grinev-nikk@yandex.ru>
2022-10-09 01:15:01 +0300

    sprint 5, current work
    add FindAndPrint function

--- printrange/PrintRange.cpp
+++ printrange/PrintRange.cpp
@@ -13,20 +13,68 @@ void PrintRange(It range_begin, It range_end) {
             cout << *it << " "s;
         }
 
-        cout << endl;
     }
+    cout << endl;
+}
+
+template <typename С, typename P>
+void FindAndPrint(С container, P pivot) {
+    auto start = container.begin();
+    auto end = container.end();
+
+    if (start != end && *start != pivot) {
+        for (auto element : container) {
+            if (element == pivot) {
+                break;
+            }
+            cout << element << " "s;
+            ++start;
+        }
+
+        cout << endl;
+        
+    }
+    
+    PrintRange(start, end);
 }
 
 int main() {
-    cout << "Test1"s << endl;
     set<int> test1 = {1, 1, 1, 2, 3, 4, 5, 5};
-    PrintRange(test1.begin(), test1.end());
-    cout << "Test2"s << endl;
-    vector<int> test2 = {}; // пустой контейнер
-    PrintRange(test2.begin(), test2.end());
-    vector<int> test3 = {10, 9, 8};
-    PrintRange(test3.begin(), test3.end());
-    string test4 = "abacaba";
-    PrintRange(test4.begin(), test4.end());
+    cout << "Test1.1"s << endl;
+    FindAndPrint(test1, 3);
+    cout << "Test1.2"s << endl;
+    FindAndPrint(test1, 0); // элемента 0 нет в контейнере
+    vector<int> test2 = {10, 9, 8};
+    cout << "Test2.1"s << endl;
+    FindAndPrint(test2, 9);
+    cout << "Test2.2"s << endl;
+    FindAndPrint(test2, 10);
+    cout << "Test2.3"s << endl;
+    FindAndPrint(test2, 8);
+    cout << "Test2.4"s << endl;
+    FindAndPrint(test2, 7);
+    string test3 = "abacabn";
+    cout << "Test3.1"s << endl;
+    FindAndPrint(test3, 'a');
+    cout << "Test3.2"s << endl;
+    FindAndPrint(test3, 'n');
+    cout << "Test3.3"s << endl;
+    FindAndPrint(test3, 'g');
+    vector<int> test4 = {}; // пустой контейнер
+    cout << "Test4.1"s << endl;
+    FindAndPrint(test4, 0);
+    cout << "Test4.2"s << endl;
+    FindAndPrint(test4, 1);
+    cout << "Test4.3"s << endl;
+    FindAndPrint(test4, 2);
+    vector<double> test5 = {10.1, 9.2, 8.3};
+    cout << "Test5.1"s << endl;
+    FindAndPrint(test5, 10.1);
+    cout << "Test5.2"s << endl;
+    FindAndPrint(test5, 9.2);
+    cout << "Test5.3"s << endl;
+    FindAndPrint(test5, 8.3);
+    cout << "Test5.4"s << endl;
+    FindAndPrint(test5, 12.1);
     cout << "End of tests"s << endl;
 }
